ebb91592dfd800a8283dc406a2f0ef6265e986cf
Maycon Bordin <mayconbordin@gmail.com>
2015-06-22 15:29:56 -0300

    Added two helpers for localization

--- src/Mayconbordin/L5Mustache/MustacheEngine.php
+++ src/Mayconbordin/L5Mustache/MustacheEngine.php
@@ -4,6 +4,8 @@ use Illuminate\View\Engines\EngineInterface;
 use Illuminate\Filesystem\Filesystem;
 use Mustache_Engine;
 
+use Lang;
+
 class MustacheEngine implements EngineInterface {
 
 	public function __construct(Filesystem $files)
@@ -15,8 +17,13 @@ class MustacheEngine implements EngineInterface {
 	{
 		$view = $this->files->get($path);
 		$app = app();
-		$m = new Mustache_Engine( $app['config']->get('l5-mustache') );
- 
+
+        $config = $app['config']->get('l5-mustache');
+
+        $config['helpers'] = array_merge($this->loadLaravelHelpers(), isset($config['helpers']) ? $config['helpers'] : []);
+
+		$m = new Mustache_Engine($config);
+
  		$data = array_map(function($item){
 			return (is_object($item) && method_exists($item, 'toArray')) ? $item->toArray() : $item;
 		}, $data);
@@ -24,4 +31,23 @@ class MustacheEngine implements EngineInterface {
 		return $m->render($view, $data);
 	}
 
+    protected function loadLaravelHelpers()
+    {
+        return [
+            'lang' => function($key) {
+                return Lang::get($key);
+            },
+
+            'choice' => function($key) {
+                $args = explode('|', $key);
+
+                if (sizeof($args) < 2) {
+                    $args[1] = 1;
+                }
+
+                return Lang::choice($args[0], $args[1]);
+            }
+        ];
+    }
 }
+
